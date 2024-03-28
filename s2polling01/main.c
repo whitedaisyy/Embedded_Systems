e7e5ced8d0ce70562eeb250ab0d9a627e22e9909
whitedaisyy <163875848+whitedaisyy@users.noreply.github.com>
2024-03-28 14:58:58 +0100

    Add files via upload

---
+++ s2polling01/main.c
@@ -0,0 +1,23 @@
+#include <msp430.h>
+
+void main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
+
+    P1DIR &= ~BIT3;                         // Set P1.3 as input
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+
+    while (1)                               // Test P1.3
+    {
+        if (P1IN & BIT3)
+            P1OUT |= BIT0;                  // if P1.3 set, set P1.0
+    else
+        P1OUT &= ~BIT0;                     // else reset
+    }
+}
+
