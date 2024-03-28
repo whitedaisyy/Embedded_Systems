f33139d644bab387976a3c76d305749faca6f703
whitedaisyy <163875848+whitedaisyy@users.noreply.github.com>
2024-03-28 14:58:03 +0100

    Add files via upload

---
+++ BlinkenTaster.c/Debug/msp430fr235x_P1_01.d
@@ -0,0 +1,54 @@
+# FIXED
+
+msp430fr235x_P1_01.obj: ../msp430fr235x_P1_01.c
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/msp430.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/msp430fr2355.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/in430.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/legacy.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/inttypes.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_inttypes.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+msp430fr235x_P1_01.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+
+../msp430fr235x_P1_01.c:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/in430.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/legacy.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/inttypes.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_inttypes.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
